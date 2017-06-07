11c2a6245b2103d063ab9d6d49885665b5f1bf62
ShenDezhou <bangtech@sina.com>
2017-06-07 20:04:02 +0800

    add wechat secret

--- docker/start.sh
+++ docker/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -v /search/PyCRM/src:/search --net=host -e 80:80 66d6901b944e /search/index.py --port=80
+nohup docker run -v /search/PyCRM/src:/search --net=host -e 80:8890 66d6901b944e /search/index.py --port=8890 2>&1 1>/dev/null &
